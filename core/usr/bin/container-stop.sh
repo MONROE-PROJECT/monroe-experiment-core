a52e257e44737f98abd4c622e20818029d5a9e8f
Jonas Karlsson <jonas.karlsson@kau.se>
2020-03-26 10:23:04 +0100

    NEAT BPF support

--- core/usr/bin/container-stop.sh
+++ core/usr/bin/container-stop.sh
@@ -13,9 +13,10 @@ MNS="ip netns exec monroe"
 NEAT_CONTAINER_NAME=monroe-neat-proxy
 EXCLUDED_IF="Br|lo|metadata|wwan|ifb|docker"
 OPINTERFACES="nlw_"
-URL_NEAT_PROXY=monroe/neat-proxy
+URL_NEAT_PROXY="monroe/neat-proxy"
+NEAT_CG_DIR="/tmp/cgroupv2/neat-proxy"
 
-# Update above default variables if needed 
+# Update above default variables if needed
 . /etc/default/monroe-experiments
 
 _tmpfile_=/tmp/cleanup-$SCHEDID.log
@@ -44,7 +45,7 @@ _UPDATE_FIREWALL_="0"
   /usr/bin/usage-defaults
 }
 
-if [ docker inspect $CONTAINER_NAME &>/dev/null ]; then 
+if [ docker inspect $CONTAINER_NAME &>/dev/null ]; then
   echo -n "Stopping container... "
   if [ "$(docker inspect -f "{{.State.Running}}" $CONTAINER_NAME) &>/dev/null" == "true" ]; then
     docker stop --time=10 $CONTAINER_NAME
@@ -66,13 +67,13 @@ fi
 
 if [[ -f $VM_OS_DISK ]]; then # This file should always be here normaly
   echo -n "Deleting OS disk... "
-  rm -f $VM_OS_DISK 
+  rm -f $VM_OS_DISK
   echo "ok."
 fi
 
-if [[ -f $VM_TMP_FILE ]]; then # This file should NOT be here normaly 
+if [[ -f $VM_TMP_FILE ]]; then # This file should NOT be here normaly
   echo -n "Deleting ramdisk file... "
-  rm -f $VM_TMP_FILE 
+  rm -f $VM_TMP_FILE
   echo "ok."
 fi
 
@@ -80,7 +81,7 @@ if [ -f $_EXPPATH.vmifhash ];then
   VMIFHASH="$(cat $_EXPPATH.vmifhash) 2>/dev/null" || true
   if [ ! -z "$VMIFHASH" ]; then
     VTAPS=$($MNS ls /sys/class/net/|grep "${VTAPPREFIX}${VMIFPREFIX}-") || true
-    if [ ! -z "$VTAPS" ]; then 
+    if [ ! -z "$VTAPS" ]; then
       echo -n "Deleting vtap interfaces in $MNS..."
       for IFNAME in $VTAPS; do
         echo -n "${IFNAME}..."
@@ -105,15 +106,18 @@ fi
 if [ ! -z "$NEAT_PROXY"  ] && [ -x /usr/bin/monroe-neat-init ]; then # If this is a experiment using the neat-proxy
   rm -f /etc/circle.d/60-*-neat-proxy.rules
   _UPDATE_FIREWALL_="1"
-  ## Stop the neat proxy container 
-  docker rm -f $NEAT_CONTAINER_NAME
+  ## Stop the neat proxy container
+  docker stop $NEAT_CONTAINER_NAME || echo "Could not stop $NEAT_CONTAINER_NAME"
+  docker rm -f $NEAT_CONTAINER_NAME || echo "Could not remove $NEAT_CONTAINER_NAME"
+  umount ${NEAT_CG_DIR} &>/dev/null || echo "Could not umount ${NEAT_CG_DIR}"
+  rm -rf ${NEAT_CG_DIR} &>/dev/null 
 fi
 
 echo -n "Syncing traffic statistics... "
 TRAFFIC=$(cat $_EXPPATH.traffic)
 
 # Will only exist if /usr/bin/usage has been running
-if [ -d $USAGEDIR/$SCHEDID ];then 
+if [ -d $USAGEDIR/$SCHEDID ];then
   for i in $(ls $USAGEDIR/$SCHEDID/*.rx.total|sort); do
     MACICCID=$(basename $i | sed -e 's/\..*//g')
     TRAFFIC=$(echo "$TRAFFIC" | jq ".interfaces.\"$MACICCID\"=$(cat $USAGEDIR/$SCHEDID/$MACICCID.total)")
@@ -127,7 +131,7 @@ if [ ! -z "$TRAFFIC" ]; then
 fi
 echo "ok."
 
-#TODO: Make this dynamic to not hardcode to wlan0  
+#TODO: Make this dynamic to not hardcode to wlan0
 EDUROAM_PID="$(pgrep -f /sbin/wpa_supplicant 2>/dev/null)"
 if [ ! -z "$EDUROAM_IDENTITY" ] && \
    [ -x /usr/bin/eduroam-login.sh ] && \
@@ -140,7 +144,7 @@ if [ ! -z "$EDUROAM_IDENTITY" ] && \
     ifconfig wlan0 0.0.0.0 down
     echo "ok."
 fi
-## Retrive Kernel Logs 
+## Retrive Kernel Logs
 if [ ! -z "$STARTTIME" ]; then
   # TODO : USe docker inspect to get time when container was running
   echo "Retrieving dmesg events:"
@@ -153,7 +157,7 @@ fi
 cat $_tmpfile_ > $_EXPPATH/cleanup.log
 echo "(end of public log)"
 
-## Cleanup 
+## Cleanup
 echo -n "Deleting container image... "
 REF=$( docker images | grep $CONTAINER_NAME | awk '{print $3}' )
 if [ -z "$REF" ]; then
@@ -164,17 +168,17 @@ fi
 echo "ok."
 
 
-##WEBGUI status codes 
+##WEBGUI status codes
 if [ -z "$STATUS" ]; then
   echo 'stopped' > $_EXPPATH.status;
 else
   echo $STATUS > $_EXPPATH.status;
 fi
 
-#Signal to sync script that it can clean the folders (when synched) 
+#Signal to sync script that it can clean the folders (when synched)
 touch $_EXPPATH.stopped
 
-if [ -x /usr/bin/modems ]; then 
+if [ -x /usr/bin/modems ]; then
   echo -n "Resetting modem state: "
   for ip4table in $(modems|jq .[].ip4table); do
     curl -s -X POST http://localhost:88/modems/${ip4table}/usbreset
@@ -182,7 +186,7 @@ if [ -x /usr/bin/modems ]; then
   echo "done"
 fi
 echo "ok."
-if [ "$_UPDATE_FIREWALL_" -eq "1" ];then 
+if [ "$_UPDATE_FIREWALL_" -eq "1" ];then
   echo -n "Restarting firewall: "
   circle start
 fi
